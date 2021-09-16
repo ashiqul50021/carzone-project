28a1cd55dbc778231dfa2033d2e21a553520e754
ashiqul50021 <ashiqulislamrasel@gmail.com>
2021-09-16 21:37:09 +0600

    heroku settings

--- carzone/settings.py
+++ carzone/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 # from pathlib import dj_database_url
 import dj_database_url
 import django_heroku
+django_heroku.settings(locals())
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
